6a6753f82a54ae1e758962c28ea8fdfc58bcf078
mariamantar <mariamantar1@hotmail.com>
2019-01-16 23:45:27 +1100

    add jquery scrolls

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-
+// sticky nav
   $('.js--section-features').waypoint(function(direction) {
     if (direction == "down") {
       $('nav').addClass('sticky');
@@ -7,4 +7,14 @@ $(document).ready(function(){
       $('nav').removeClass('sticky');
     }
   });
+
+// scroll on buttons
+$('.js--scroll-to-plans').click(function () {
+  $('html, body').animate({scrollTop: $('.js--info-section').offset().top}, 1000);
+});
+
+$('.js--scroll-to-start').click(function () {
+  $('html, body').animate({scrollTop: $('.js--section-features').offset().top}, 1000);
+});
+
 });
